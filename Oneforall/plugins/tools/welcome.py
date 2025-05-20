baa1194f8dba58db058694fe276c4210f3a99e21
vagetatg <nibbanmisal3302@gmail.com>
2025-05-21 01:37:56 +0530

    Update welcome.py

--- Oneforall/plugins/tools/welcome.py
+++ Oneforall/plugins/tools/welcome.py
@@ -1,13 +1,12 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from pymongo import MongoClient
-from Oneforall import app
 
 mongo = MongoClient("mongodb+srv://I-LOVE-PDF-BOT:I-LOVE-PDF-BOT@cluster0.c51o3a9.mongodb.net/?retryWrites=true&w=majority")  
 db = mongo["telegram_bot"]
 welcome_col = db["welcome_messages"]
 
-@app.on_message(filters.command("setwelcome") & filters.group)
+@Client.on_message(filters.command("setwelcome") & filters.group)
 async def set_welcome(client, message: Message):
     if len(message.command) < 2:
         return await message.reply("Usage:\n/setwelcome Your welcome message here.")
@@ -22,7 +21,7 @@ async def set_welcome(client, message: Message):
     )
     await message.reply("Custom welcome message has been set!")
 
-@app.on_message(filters.command("welcome") & filters.group)
+@Client.on_message(filters.command("welcome") & filters.group)
 async def toggle_welcome(client, message: Message):
     if len(message.command) >= 2 and message.command[1].lower() == "off":
         chat_id = message.chat.id
@@ -35,7 +34,7 @@ async def toggle_welcome(client, message: Message):
     else:
         await message.reply("Use `/welcome off` to disable welcome system.", quote=True)
 
-@app.on_message(filters.new_chat_members & filters.group)
+@Client.on_message(filters.new_chat_members & filters.group)
 async def welcome_new_member(client, message: Message):
     chat_id = message.chat.id
     data = welcome_col.find_one({"chat_id": chat_id, "enabled": True})
